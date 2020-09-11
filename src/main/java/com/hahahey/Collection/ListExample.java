905cb27f65af0897283e42cac07596e8f6200af9
lizhongkai <lizhongkai@cnhqd.net>
2020-09-11 17:50:31 +0800

    modify the  collection example

--- src/main/java/com/hahahey/Collection/ListExample.java
+++ src/main/java/com/hahahey/Collection/ListExample.java
@@ -1,10 +1,6 @@
 package com.hahahey.Collection;
 
-/**
- * @author lizhongkai
- * @description
- * @date 2020-09-11
- */
+
 public class ListExample {
     public static void main(String[] args) {
 
