13b4357b2414058bad369fe5d898601b9f437b51
lizhongkai <lizhongkai@cnhqd.net>
2020-09-24 16:51:54 +0800

    新增博客整理

---
+++ src/main/java/com/hahahey/IO.java
@@ -0,0 +1,26 @@
+package com.hahahey;
+
+
+public class IO {
+
+   /**
+    *                     数据传输
+    *
+    *           输入流                 输出流
+    *     字节流       字符流     字节流      字符流
+    *  InputStream    Reader  OutPutStream  Writer
+    *
+    *
+    *
+    *
+    *
+    *
+    *
+    * 参考资料: https://www.cnblogs.com/LiaHon/p/11924849.html
+    **/
+
+
+    public static void main(String[] args) {
+
+    }
+}
