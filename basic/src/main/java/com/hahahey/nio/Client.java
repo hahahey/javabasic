6e4fdb5d6f960dfb42ad4ccfa739206cbb52cb32
hahahey <15094025379@163.com>
2024-08-10 23:19:37 +0800

    add java nio

--- basic/src/main/java/com/hahahey/nio/Client.java
+++ basic/src/main/java/com/hahahey/nio/Client.java
@@ -2,8 +2,12 @@ package com.hahahey.nio;
 
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * @author hahahey
@@ -11,17 +15,53 @@ import java.util.Scanner;
  */
 public class Client {
     public static void main(String[] args) throws Exception {
-        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8888));
+
+        Selector selector = Selector.open();
+        SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(8888));
         socketChannel.configureBlocking(false);
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
-        Scanner scanner = new Scanner(System.in);
+//        ByteBuffer buffer = ByteBuffer.allocate(1024);
+//        Scanner scanner = new Scanner(System.in);
+//        while (true) {
+//            System.out.print("请输入:");
+//            String msg = scanner.nextLine();
+//            buffer.put(msg.getBytes());
+//            buffer.flip();
+//            socketChannel.write(buffer);
+//            buffer.clear();
+//
+//            System.out.println("-----");
+//        }
+
+
+        socketChannel.register(selector, SelectionKey.OP_CONNECT | SelectionKey.OP_READ);
+
+
+
         while (true) {
-            System.out.print("请输入:");
-            String msg = scanner.nextLine();
-            buffer.put(msg.getBytes());
-            buffer.flip();
-            socketChannel.write(buffer);
-            buffer.clear();
+            selector.select();
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                System.out.println("------------------");
+                SelectionKey selectionKey = iterator.next();
+                iterator.remove();
+                if (selectionKey.isConnectable()) {
+
+                    //System.out.println(socketChannel.finishConnect());
+
+
+                }else if(selectionKey.isReadable()){
+                    SocketChannel channel = (SocketChannel)selectionKey.channel();
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+                    byteBuffer.clear();
+                    channel.read(byteBuffer);
+                    byteBuffer.flip();
+                    System.out.println(new String(byteBuffer.array()));
+                }
+                //iterator.remove();
+            }
+
         }
+
+
     }
 }
--- basic/src/main/java/com/hahahey/nio/Server.java
+++ basic/src/main/java/com/hahahey/nio/Server.java
@@ -13,6 +13,12 @@ import java.util.Iterator;
  * @date 2023-04-02 18:32
  */
 public class Server {
+    /**
+     * connect 客户端连接成功时触发
+     * accept  服务端成功接收连接时触发
+     * read 数据可读入时触发
+     * write 数据可写入时触发
+     */
     public static void main(String[] args) throws Exception {
 
         //1.获取管道
@@ -25,38 +31,69 @@ public class Server {
         Selector selector = Selector.open();
         //5.将通道注册到选择器上，并且开始指定监听的接收事件
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        System.out.println("服务端已启动...");
         //6.轮询已经就绪的事件
-        while (selector.select() > 0){
-            System.out.println("开启事件处理");
+        while (true) {
+            //阻塞等待channel就绪
+            selector.select();
+            System.out.println("received message!!!!!!!!!!!!!!!!!!!!!!!!!!");
             //7.获取选择器中所有注册的通道中已准备好的事件
             Iterator<SelectionKey> it = selector.selectedKeys().iterator();
             //8.开始遍历事件
-            while (it.hasNext()){
-                SelectionKey selectionKey = it.next();
-                System.out.println("--->"+selectionKey);
+            while (it.hasNext()) {
+                SelectionKey key = it.next();
                 //9.判断这个事件具体是啥
-                if (selectionKey.isAcceptable()){
+                if (key.isAcceptable()) {
+                    System.out.println("客户端已连接");
                     //10.获取当前接入事件的客户端通道
                     SocketChannel socketChannel = serverSocketChannel.accept();
                     //11.切换成非阻塞模式
                     socketChannel.configureBlocking(false);
                     //12.将本客户端注册到选择器
-                    socketChannel.register(selector,SelectionKey.OP_READ);
-                }else if (selectionKey.isReadable()){
-                    //13.获取当前选择器上的读
-                    SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
-                    //14.读取
-                    ByteBuffer buffer = ByteBuffer.allocate(1024);
-                    int len;
-                    while ((len = socketChannel.read(buffer)) > 0){
-                        buffer.flip();
-                        System.out.println(new String(buffer.array(),0,len));
-                        //清除之前的数据（覆盖写入）
-                        buffer.clear();
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+
+                    //向客户端发送消息
+                    ByteBuffer buffer = ByteBuffer.wrap("message from server ".getBytes());
+                    socketChannel.write(buffer);
+
+                    //当缓冲区至少还有一个元素时，返回 true
+                    if (buffer.hasRemaining()) {
+                        //添加写事件
+                        key.interestOps(key.interestOps() + SelectionKey.OP_WRITE);
+                        //把写的数据挂到channel上
+                        key.attach(buffer);
                     }
+                } else if (key.isReadable()) {
+                    try {
+                        SocketChannel readChannel = (SocketChannel) key.channel();
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+                        readChannel.read(byteBuffer);
+                        byteBuffer.flip();
+                        System.out.println("接收到客户端数据 " + new String(byteBuffer.array()));
+
+                    } catch (Exception e) {
+                        System.out.println("客户端已退出..........");
+                    } finally {
+                        // 客户端发送数据过来，正常，异常关闭都会触发read事件 所以在正常关闭时候需要取消在selector上注册的channel，后续不在监听
+                        key.cancel();
+                    }
+                } else if (key.isWritable()) {
+                    ByteBuffer writeByteBuffer = (ByteBuffer) key.attachment();
+                    SocketChannel channel = (SocketChannel) key.channel();
+                    channel.write(writeByteBuffer);
+
+                    if (!writeByteBuffer.hasRemaining()) {
+                        System.out.println("服务器已发送消息...取消写事件");
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);
+                        key.attach(null);
+                    }
+
+                } else {
+
                 }
                 //15.处理完毕后，移除当前事件
-                it.remove();
+                //it.remove();
             }
         }
 
